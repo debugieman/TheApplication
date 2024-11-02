8438a07af044924e6a7ed1d623d7a13d9fe2a962
Debuggieman <107075050+debugieman@users.noreply.github.com>
2024-11-02 18:18:10 +0100

    UserEntity

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,13 @@
+﻿namespace API.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+
+        // all props must be public so EF can work with them
+        // in EF class is table, prop is column
+        public string UserName { get; set; }
+
+        // pascal for casing of properties
+    }
+}
--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea56755edf56716bda042fa0cd8fd35b28f3e3f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0363157f35ab42caef35994e6666e7e71193ede2")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
