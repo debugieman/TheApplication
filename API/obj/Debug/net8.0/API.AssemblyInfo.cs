418d09718ce985ab6ee88d5eea23fc21d0c5fe57
Debuggieman <wsarzynski@gmail.com>
2024-12-13 19:32:33 +0100

    adding CORS

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b30126d267884566f9d37e03da76ecb3c3effc1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10858a2cc03e7f1adecce1894361f4f5a28765cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
